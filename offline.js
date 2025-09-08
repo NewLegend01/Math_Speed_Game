405828344cf0dd9194805f3b68d522da88072836
NewLegend01 <newaera01@gmail.com>
2025-09-09 02:37:08 +0700

    Math Speed Game upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1757360061,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bghome-sheet0.png",
+		"images/bgmn-sheet0.png",
+		"images/bgnn-sheet0.png",
+		"images/bgl1-sheet0.png",
+		"images/bgl2-sheet0.png",
+		"images/bgl3-sheet0.png",
+		"images/bgl4-sheet0.png",
+		"images/bggameover-sheet0.png",
+		"images/btnn-sheet0.png",
+		"images/bghomenext-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
